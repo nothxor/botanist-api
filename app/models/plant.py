57b7bf79166b37455263c0759a9377270df36618
Stefan Sandberg <nothxor@gmail.com>
2025-08-29 15:12:54 +0200

    Model: Plant

---
+++ app/models/plant.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Column, Integer, String, DateTime, Boolean, Float
+from sqlalchemy.ext.declarative import declarative_base
+from datetime import datetime
+
+Base = declarative_base()
+
+class Plant(Base):
+    __tablename__ = "plants"
+
+    id = Column(Integer, primary_key=True, index=True)
+    species = Column(String, nullable=False)
+    date_added = Column(DateTime, default=datetime.utcnow, nullable=False)
+    notes = Column(String, nullable=True)
+
+    def __repr__(self):
+        return f"<Plant(id={self.id}, species={self.species})>"
