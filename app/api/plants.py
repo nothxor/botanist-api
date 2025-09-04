2c22599029e6d288287864b9ff340a224560137e
Stefan Sandberg <nothxor@gmail.com>
2025-09-04 15:49:23 +0200

    Added POST for adding new plants to the database

--- app/api/plants.py
+++ app/api/plants.py
@@ -1,12 +1,21 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from app.core.database import get_db
+
 from app.models.plant import Plant
+from app.schemas.plant import PlantCreate
 
 router = APIRouter()
 
 @router.get("/plants")
 def get_all_plants(db: Session = Depends(get_db)):
-    plants = db.query(Plant).all
+    plants = db.query(Plant).all()
     return plants
 
+@router.post("/plants")
+def create_new_plant(plant: PlantCreate, db: Session = Depends(get_db)):
+    new_plant = Plant(species=plant.species, notes=plant.notes)
+    db.add(new_plant)
+    db.commit()
+    db.refresh(new_plant)
+    return new_plant
