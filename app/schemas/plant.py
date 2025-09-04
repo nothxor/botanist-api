e2ef72de31c0513d2d07affb1eb3cc5b787bfa73
Stefan Sandberg <nothxor@gmail.com>
2025-09-04 21:20:40 +0200

    Updated Plant schema for PUT endpoint

--- app/schemas/plant.py
+++ app/schemas/plant.py
@@ -1,5 +1,10 @@
+from typing import Optional
 from pydantic import BaseModel
 
 class PlantCreate(BaseModel):
     species: str
     notes: str = None
+
+class PlantUpdate(BaseModel):
+    species: Optional[str] = None
+    notes: Optional[str] = None
